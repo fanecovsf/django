b9d65e105c5fdea16419a11810abd66fcda3ac46
fanecovsf <122293280+fanecovsf@users.noreply.github.com>
2023-05-23 06:56:12 -0300

    Usando tag block e extensão de arquivo, testando cards de receitas

--- projeto-curso/recipes/views.py
+++ projeto-curso/recipes/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from utils.factory import make_recipe
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name':'Gustavo'
+        'recipes':[make_recipe() for _ in range(10)]
     })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name':'Gustavo'
+        'recipe':[make_recipe()]
     })
